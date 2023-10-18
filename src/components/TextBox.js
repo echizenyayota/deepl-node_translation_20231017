6037c6ebf84e242caffc60427c49ea55b64747bd
echizenyayota <virtualmisso30@gmail.com>
2023-10-18 18:07:22 +0900

    make the text area in the TextBox component

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,7 +1,10 @@
-const TextBox = () => {
+const TextBox = ({ style }) => {
   return (
     <div>
-      TextBox
+      <textarea 
+        placeholder={style === "input" ? "Enter Text" : "translation"}
+        disabled={style === "output"}
+      />
     </div>
   )
 };
