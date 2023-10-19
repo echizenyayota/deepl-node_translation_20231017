03949cc92f5679f74d8772e4face19340fba317b
echizenyayota <virtualmisso30@gmail.com>
2023-10-19 19:22:06 +0900

    display the strings in the modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Modal = () => {
+
+  const [searchedLanguage, setSearchedLanguage] = useState("");
+
+  const handleClick = (e) => {
+    setSearchedLanguage(e.target.value);
+  }
+
+  console.log(searchedLanguage);
+
   return (
-    <div>
-      Modal
+    <div className="option-List">
+      <div className="search-bar">
+        <input value={searchedLanguage} onChange={handleClick} />
+      </div>
     </div>
   )
 };
