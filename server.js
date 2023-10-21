4a7b5e0371a9046f4e15b0aabd74eba302280680
echizenyayota <virtualmisso30@gmail.com>
2023-10-21 19:43:43 +0900

    install deepl-node and add codes to use the package(failed)

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+import * as deepl from "deepl-node";
+
 const PORT = 8000;
 const axios = require("axios").default;
 const express = require("express");
