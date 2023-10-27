eafa59bd0d3795b214a5b29d745ab82bd0589e5c
echizenyayota <virtualmisso30@gmail.com>
2023-10-27 18:43:23 +0900

    Translate the text in the textarea

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -15,11 +15,16 @@ const TextBox = ({
         style={style} 
         selectedLanguage={selectedLanguage}
         setShowModal={setShowModal}
+        onChange={(e) => setTextToTranslate(e.target.value)}
+        value={style === "input" ? textToTranslate : translatedText}
       />
       <textarea 
         placeholder={style === "input" ? "Enter Text" : "translation"}
         disabled={style === "output"}
       />
+      {style === "input" && (
+        <div className="delete" onClick={handleClick}>x</div>
+      )}
     </div>
   )
 };
